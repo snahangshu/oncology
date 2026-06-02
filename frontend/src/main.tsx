8cc3ef6cd0ea76d430688bb5725c337664bcc201
Sanjeet Verma <vermasanjeet240@gmail.com>
2026-06-02 11:32:42 +0530

    fix: add QueryClientProvider to main.tsx for react-query hooks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./app/store";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
-  
